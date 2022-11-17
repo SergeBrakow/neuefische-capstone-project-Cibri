4831863ce0ace02a3dcf5ab233fb7ce418a250b5
Serge <45800906+SergeBrakow@users.noreply.github.com>
2022-11-17 10:28:10 +0100

    create Order Page without the form

--- src/pages/CreateOrder.js
+++ src/pages/CreateOrder.js
@@ -1,16 +1,31 @@
+import { useNavigate, useParams} from "react-router-dom"; 
+import { useState } from "react";
 import styled from "styled-components";
 
 import NavBarNewOrder from "../components/NavBarNewOrder";
+import EditOrderForm from "../components/EditOrderForm";
+import { getDateString } from "../utils/getDate";
 
 export default function CreateOrder({onHandleSubmit}){
-   
+    const navigate = useNavigate(); 
+
+    let {dateString} = useParams();
+    if(dateString === undefined || dateString.length !== 10) {
+        dateString = getDateString(new Date());
+    }
+    
+    const showDate = new Date(Number(dateString.slice(6, 10)),
+                              (Number((dateString.slice(3, 5)-1))), 
+                                Number(dateString.slice(0, 2)));
+    
+    const [orderDate, setOrderDate] = useState(showDate);
 
     return(
         <>
             <StyledHead>
                 <p>erstelle neuen Eintrag </p>
             </StyledHead> 
-
+            <EditOrderForm date={orderDate} onHandleSubmit={onHandleSubmit}/>
             <NavBarNewOrder page={"new"}/>
         </>
     );
