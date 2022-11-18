23c5dd18e49f8dacd20e9a238c811d775efbecd1
Serge <45800906+SergeBrakow@users.noreply.github.com>
2022-11-18 05:56:37 +0100

    edit view page / create viewselection with navigation

--- src/pages/ViewOrder.js
+++ src/pages/ViewOrder.js
@@ -1,11 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
-export default function ViewOrder(){
-    const navigate = useNavigate();
+import NavBarNewOrder from "../components/NavBarNewOrder";
+import ViewOrderSection from "../components/ViewOrderSection";
+
+export default function ViewOrder({entries}){
+    let {id} = useParams();
+    const order = entries.find( (entry) => entry.id === id);
+
     return(
         <>
-            <p>Einträge können aktuell noch nicht bearbeitet/angeschaut werden!</p>
-            <button onClick={()=> navigate("/home")}>zurück</button>
+        <StyledHead>
+            <p>Detailansicht</p>
+        </StyledHead> 
+        <ViewOrderSection order={order} />
+        <NavBarNewOrder page={"new"}/>
         </>
     );
 }
+
+export const StyledHead = styled.div`
+    border-bottom: 1px solid;
+    background-color: white;
+    position: fixed;
+    width: 100%;
+    height: 70px;
+    top: 0;
+    layer: 10; 
+    margin-bottom: 10px; 
+`
