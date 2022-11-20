67cfd0ba67bdb917e82ec967d400fc4b62cf15ed
Serge <45800906+SergeBrakow@users.noreply.github.com>
2022-11-20 16:44:14 +0100

    update to order view page

---
+++ src/components/OrderViewSection.js
@@ -0,0 +1,83 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { getTimeAsString } from "../utils/getDate";
+import { MainBox } from "./styles/MainBox";
+
+export default function OrderViewSection({userList, order}){
+    const navigate = useNavigate();
+    
+    window.scrollTo(0, 0);     
+    return (
+        <MainBox>
+            <div>
+                <Name>Typ:</Name>
+                <P>{order.type}</P>
+                <Name>Titel: </Name>
+                <P>{order.name}</P>
+                <Name >Zeit</Name>
+                <P>{getTimeAsString(order.date.hour, order.date.minute)}</P>
+                <Name >Datum</Name>
+                <P>{order.date.dateString}</P>
+                <Name >Notizen</Name>
+                {order.note ===""?
+                    <P>-</P> :  <P>{order.note}</P>
+                }
+                <Name >verlinkte Nutzer</Name>
+                {order.linkedUser.length === 0? (
+                    <P>-</P> 
+                    ) : (
+                        order.linkedUser.map((linkedUserId) => 
+                            <ABtn key={linkedUserId}
+                                onClick={() =>{ navigate(`/userView/${linkedUserId}`)}}
+                                >{userList.find((user) => user.id === linkedUserId).name}</ABtn>
+                            // <P key={linkedUserId}>{userList.find((user) => user.id === linkedUserId).name}</P>
+                        )
+                    )
+                }
+            </div>
+            <Button onClick={()=> navigate(`/orderEdit/${order.id}`)}>bearbeiten</Button>
+        </MainBox>
+    );
+}
+
+const Name = styled.p`
+    font-weight: bold; 
+`
+
+const P=styled.p`
+    margin-top: -10px;
+    padding: 5px 15px;
+    background-color: #ececed;
+    border-radius: 15px;
+`
+const ABtn=styled.a`
+    margin: 5px;
+    margin-top: -10px;
+    padding: 5px 15px;
+    border: 1px solid;
+    border-radius: 15px;
+    background-color: #f4f9f4;
+    min-width: 45px;
+    height: 25px;
+    cursor: pointer;
+    &: hover {
+        background-color: white;
+        border: none;
+        -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.85); 
+        box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.85);
+    }
+`
+const Button= styled.button`
+    height: 40px;
+    width: 90px;
+    margin-top: 20px;
+    background-color: #f4f9f4;
+    border: 1px solid;
+    border-radius:10px;
+    &: hover {
+        background-color: white;
+        border: none;
+        -webkit-box-shadow: 0px 0px 24px 5px rgba(0,0,0,0.38); 
+        box-shadow: 0px 0px 24px 5px rgba(0,0,0,0.38);
+    }   
+`
