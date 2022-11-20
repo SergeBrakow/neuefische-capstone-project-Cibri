03fb704b4766721687c927e98d3b496169c39fd8
Serge <45800906+SergeBrakow@users.noreply.github.com>
2022-11-20 16:37:36 +0100

    use USerContext CRUD functionality s now in orderEdir/ orderCreate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { UserContext } from "./utils/UserContext";
 
 import Layout from "./components/Layout";
 import ErrorPage from "./pages/ErrorPage";
@@ -7,10 +8,12 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import New from "./pages/New";
 import Customer from "./pages/Customer"
-import EditOrder from "./pages/EditOrder";
-import ViewOrder from "./pages/ViewOrder";
+import OrderCreate from "./pages/OrderCreate";
+import OrderView from "./pages/OrderView";
+import OrderEdit from "./pages/OrderEdit";
 import { toLocalStorage, fromLocalStorage } from "./utils/localStorage";
-import CreateOrder from "./pages/CreateOrder";
+import UserView from "./pages/UserView";
+import UserLinkedOrderView from "./pages/UserLinkedOrderView";
 
 
 
@@ -19,6 +22,7 @@ function App() {
   const [customerList, setCustomerList] = useState(fromLocalStorage("customerList"));
   const [userList, setUserList] = useState(fromLocalStorage("userList"));
   const [user, setUser] = useState(fromLocalStorage("loggedUser"));
+  const [idPosInHome, setIdPosInHome] = useState(0);
   document.title = "Cibri";
   
   useEffect(() => {
@@ -27,99 +31,84 @@ function App() {
 
   useEffect(() => {
     toLocalStorage("customerList", customerList);
-  }, [orderList]);
+  }, [customerList]);
 
   useEffect(() => {
     toLocalStorage("userList", userList);
-  }, [orderList]);
+  }, [userList]);
 
-  function addOrder(orderId, order_type, order_name, orderDateFull, note){
-    setOrderList([
-      {
-        id: orderId,
-        type: order_type,
-        name: order_name,
-        date: orderDateFull,
-        owner: user.name,
-        note: note,
-      },
-      ...orderList,
-    ]);
-  }
+  useEffect(() => {
+    toLocalStorage("loggedUser", user);
+  }, [user]);
 
-  function editOrder(orderId, order_type, order_name, orderDateFull, note){
-    if(order_name === "") {
-      deleteOrder(orderId);
-    } else {
-      setOrderList(
-        orderList.map ((order) =>
-          order.id === orderId ? {
-            ...order,
-            type: order_type,
-            name: order_name,
-            date: orderDateFull,
-            owner: user.name,
-            note: note,
-          } : 
-            order
-        )
-      );
-    }
-  }
-
-  function deleteOrder(id){
-    setOrderList(orderList.filter((order) => order.id !== id));
-  }
-
+  
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index 
-          element={<Login setUser={setUser}/>}
-        />
-        <Route
-          path="home/:dateString"
-          element={<Home  
-            user={user}
-            entries={orderList}/>}
-        />
-        <Route
-          path="home"
-          element={<Home  
-            user={user}
-            entries={orderList}/>}
-        />
-         <Route
-          path="new/:dateString"
-          element={<New />}
-        />
-        <Route
-          path="createOrder/:dateString"
-          element={<CreateOrder
-            onHandleSubmit={addOrder}/>}
-        />
-        <Route
-          path="viewOrder/:id"
-          element={<ViewOrder 
-            entries={orderList}/>}
-        />
-        <Route
-          path="editOrder/:id"
-          element={<EditOrder 
-            user={user}
-            entries={orderList}
-            onHandleSubmit={editOrder}/>}
-        />
-        <Route
-          path="createCustomer"
-          element={<Customer
-            customerList={customerList}
-            setCustomerList={setCustomerList}
-            action={"create"} />}
-        />
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-    </Routes>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        userList,
+        setUserList,
+        customerList,
+        setCustomerList,
+        orderList,
+        setOrderList,
+        idPosInHome,
+        setIdPosInHome,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index 
+            element={<Login />}
+          />
+          <Route
+            path="home/:dateString"
+            element={<Home/>}
+          />
+          <Route
+            path="home"
+            element={<Home/>}
+          />
+          <Route
+            path="new/:dateString"
+            element={<New />}
+          />
+          <Route
+              path="orderView/:id"
+              element={<OrderView/>}
+            />
+          <Route
+            path="orderCreate/:dateString"
+            element={<OrderCreate/>}
+          />
+          <Route
+            path="orderEdit/:id"
+            element={<OrderEdit/>}
+          />
+          <Route
+              path="userView/:id"
+              element={<UserView/>}
+          />
+          <Route
+              path="userLinked/"
+              element={<UserLinkedOrderView/>}
+          />
+          <Route
+              path="userLinked/:id"
+              element={<UserLinkedOrderView/>}
+          />
+          <Route
+            path="createCustomer"
+            element={<Customer
+              customerList={customerList}
+              setCustomerList={setCustomerList}
+              action={"create"} />}
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 }
 
