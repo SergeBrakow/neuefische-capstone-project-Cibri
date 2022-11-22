7f683051ab2323a4ed2223a9dc68f3cadea25e7b
Serge <45800906+SergeBrakow@users.noreply.github.com>
2022-11-22 11:18:46 +0100

    better code solution

--- src/components/UserLinkedInOrderSection.js
+++ src/components/UserLinkedInOrderSection.js
@@ -7,13 +7,6 @@ import { ValueBox } from "./styles/ValueBox";
 
 export default function UserLinkedInOrderSection({user, orderList}){
     const navigate = useNavigate();
-    
-    const ordersTheUserIsLinkedIn = [];
-    {orderList.filter((order) =>
-        order.linkedUser.find((linkedUser) => linkedUser === user.id) !== undefined? 
-            ordersTheUserIsLinkedIn.push(order): ""
-        )
-    }
 
     window.scrollTo(0, 0);     
     return (
@@ -21,15 +14,16 @@ export default function UserLinkedInOrderSection({user, orderList}){
             <div>
                 <Name>{user.name} ist verlinkt in:</Name>
                 <ValueBox>
-                    { ordersTheUserIsLinkedIn.map((order) =>
-                        <OrderBox 
-                            key={order.id}
-                            onClick={()=> navigate(`/orderView/${order.id}`)}
-                            >
-                            <p>{order.name}</p>
-                            <p>{order.date.dateString} - {getTimeAsString(order.date.hour, order.date.minute)}</p>
-                        </OrderBox>
-                         )
+                    {orderList.filter((orderFromOrderList) =>
+                        orderFromOrderList.linkedUser.find((linkedUser) => linkedUser === user.id) !== undefined
+                        ).map((order) => 
+                            <OrderBox 
+                                key={order.id}
+                                onClick={()=> navigate(`/orderView/${order.id}`)}
+                                >
+                                <p>{order.name}</p>
+                                <p>{order.date.dateString} - {getTimeAsString(order.date.hour, order.date.minute)}</p>
+                            </OrderBox>)
                     }
                 </ValueBox>
             </div>
